c82e2d7d67cd29dbf5831b55c5d3e4555a41183b
Juan Carlos Pacheco <n00119355@upn.pe>
2025-09-23 17:12:32 -0500

    Carpeta base para desarrollar el trabajo grupal

---
+++ S2TrabajoCampo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2TrabajoCampo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Programa 1: Declaración de variables
+            
+            //Programa 2: Promedio de notas
+            
+            //Programa 3: Nunmero par o impar
+            
+            //Programa 4: Calculadora básica
+            
+
+        }
+    }
+}
