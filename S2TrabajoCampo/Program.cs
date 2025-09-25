7a88e4b35326be7b629ee9e52a7b2e93a2de8fc5
Juan Carlos Pacheco <n00119355@upn.pe>
2025-09-24 21:30:47 -0500

    Subiendo Programa 1

--- S2TrabajoCampo/Program.cs
+++ S2TrabajoCampo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,48 @@ namespace S2TrabajoCampo
         static void Main(string[] args)
         {
 
-            //Programa 1: Declaración de variables
-            
+            //Programa 1: Conversor de temperaturas
+            double temperatura, conversion;
+            string medida, dato;
+
+            //Recogidendo variables
+            Console.WriteLine("=========================");
+            Console.WriteLine("CONVERSOR DE TEMPERATURAS");
+            Console.WriteLine("=========================");
+            Console.Write("Ingrese la temperatura en °C: ");
+            dato = Console.ReadLine();
+
+            if (double.TryParse(dato, out temperatura)) {
+                Console.WriteLine("Convertir a:");
+                Console.WriteLine("(F)ahrenheit");
+                Console.WriteLine("(K)elvin");
+                medida = Console.ReadLine().ToUpper();
+
+                if (medida == "F")
+                {
+                    conversion = (temperatura * 1.80) + 32;
+                    Console.Write("Respuesta: {0}°C equivale a {1}°F", temperatura, conversion);
+                }
+                else if (medida == "K")
+                {
+                    conversion = temperatura + 273.15;
+                    Console.Write("Respuesta: {0}°C equivale a {1} Kelvin", temperatura, conversion);
+                }
+                else {
+                    Console.WriteLine("Error: Se esperaba 'F' o 'K'. Se finalizará el programa");
+                }
+            } else {
+                Console.WriteLine("Error: Se esperaba un número. Se finalizará el programa");
+            }
+
+            Console.ReadKey();
+
             //Programa 2: Promedio de notas
-            
+
             //Programa 3: Nunmero par o impar
-            
+
             //Programa 4: Calculadora básica
-            
+
 
         }
     }
