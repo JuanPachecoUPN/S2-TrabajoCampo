c6cdd1ebb6393f5eb37312f0ef81638ffe103b02
N0479037@upn.pe <jairo_155_145@hotmail.com>
2025-09-27 19:00:54 -0500

    subiendo programa2

---
+++ Programa2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2.V
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double nota1, nota2, nota3, promedio;
+
+            Console.Write("Ingrese la primera nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la segunda nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la tercera nota: ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            // Calcular el promedio
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine($"\nEl promedio es: {promedio:F2}");
+
+            if (promedio >= 13)
+            {
+                Console.WriteLine("El estudiante está APROBADO ✅");
+            }
+            else
+            {
+                Console.WriteLine("El estudiante está DESAPROBADO ❌");
+            }
+        }
+    }
+}
+
